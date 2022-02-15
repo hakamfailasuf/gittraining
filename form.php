269ce8456210621cbe1efe657391dde917530554
hakamfailasuf <54478340+hakamfailasuf@users.noreply.github.com>
2022-02-15 20:02:59 +0700

    create additional fitur form page

---
+++ form.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Form Input</title>
+  </head>
+  <body>
+    <h3>Input Data</h3>
+    <input type=text>Name :</input>
+    <input type=date>Date :</input>
+  </body>
+</html>
+  
